63871bcd6a906c6a7b79931c2acd7f82516c8238
shahnaz khan <shahnaz@vyomr.in>
2025-05-12 20:22:57 +0530

    added few more questions

--- components/QSection.tsx
+++ components/QSection.tsx
@@ -5,6 +5,9 @@ import { PlaceholdersAndVanishInput } from "./ui/placeholders-and-vanish-input";
 export function PlaceholdersAndVanishInputDemo() {
     const placeholders = [
         "Where’s the ROI of LLM Integration?",
+        "How do I get started with LLMs?",
+        "What are the best practices for LLMs?",
+        "How to choose the right LLM for my business?",   
         "Too much spend, too little benefit?",
         "Where all I can use LLM?",
     ];
@@ -20,7 +23,7 @@ export function PlaceholdersAndVanishInputDemo() {
     return (
         <div className="h-[20rem] bg-default-950/40 backdrop-blur-md flex flex-col justify-center  items-center px-4">
             <h2 className="mb-10 sm:mb-20 text-5xl text-center sm:text-xl text-white">
-                Ask Aceternity UI Anything
+                Ask Intellium About LLMs
             </h2>
             <div className="w-full max-w-xl">
                 <PlaceholdersAndVanishInput placeholders={placeholders} onChange={handleChange} onSubmit={handleSubmit} />
